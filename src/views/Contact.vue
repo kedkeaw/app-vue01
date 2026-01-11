c62d034b933e6b1dccfb5b7de283523b0fbb4da5
kedkeaw <sukkasemkedkeaw@gmail.com>
2026-01-11 18:28:03 +0700

    update v-2

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,36 @@
 <template lang="">
-    <div>
+    <div class="container text-body-primary">
         <h1>Contact to Kedkeaw Sukkasem</h1>
         <h2>Tel 011-111-1111</h2>
-            <img alt="Vue logo" src="../assets/react.png">
+            <img alt="Vue logo" src="../assets/scenery2.jpg">
+<br>
+            <div class="container text-center mt-4">
+  <div class="row">
+    <div class="col-sm-6">
+      <img src="../assets/scenery.jpg">
+    </div>
+    <div class="col-sm-6 bg-light">
+                  <div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email address</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+<div>
+<button type="button" class="btn btn-primary me-2">Submit</button>
+<button type="button" class="btn btn-danger">Cancle</button>
+</div>
+    </div>
+  </div>
+</div>
 
     </div>
+    
 </template>
-<style lang="">
+<style scoped>
+h1 {
+  color: #302c50;
+}
 </style>
