b2754a10ac91247c3037fd65578791b50f402fa3
kedkeaw <sukkasemkedkeaw@gmail.com>
2026-01-11 16:40:05 +0700

    update v1

---
+++ src/views/Contact.vue
@@ -0,0 +1,10 @@
+<template lang="">
+    <div>
+        <h1>Contact to Kedkeaw Sukkasem</h1>
+        <h2>Tel 011-111-1111</h2>
+            <img alt="Vue logo" src="../assets/react.png">
+
+    </div>
+</template>
+<style lang="">
+</style>
